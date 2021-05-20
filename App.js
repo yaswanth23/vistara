8d98a0ce3dcd99cb759b5a2e7c39d5cca613b758
yash <yaswanth.k23@gmail.com>
2021-05-20 19:31:50 +0530

    initial set up commit

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import InitialScreen from './src/screens/InitialScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Initial: InitialScreen
   },
-});
+  {
+    initialRouteName: 'Initial',
+    defaultNavigationOptions: {
+      title: 'Vistara'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/InitialScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const InitialScreen = () => {
+    return (
+        <View>
+            <Text>Welcome to Vistara</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default InitialScreen;
