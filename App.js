99287a46029960a6dc470ab77930839e8d48a717
yash <yaswanth.k23@gmail.com>
2021-05-23 09:12:35 +0530

    inital screen design chnages

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-
+import { Asset } from 'expo';
 import SplashScreen from './src/screens/SplashScreen';
 import InitialScreen from './src/screens/InitialScreen';
 import { Router, Scene } from 'react-native-router-flux';
--- src/screens/InitialScreen.js
+++ src/screens/InitialScreen.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import {View, Image, Text, StyleSheet, Dimensions} from 'react-native';
+import { Audio, Video } from 'expo-av';
 import Animated from 'react-native-reanimated';
 
 const screenWidth = Dimensions.get('window').width;
 const screenheight = Dimensions.get('window').height;
 
 const InitialScreen = () =>{
-    
-    const val = screenheight * 25/100;
+    const val = screenheight * 0.03;
     console.log(val);
     return (
         <View style={styles.container}>
-            <Image style={styles.tinyLogo} source={require('../../res/images/mainlogo.png')}/>
-            <Text style={styles.textStyle}>Vistara</Text>
+            <View style={styles.header}>
+                <Video
+                    style={styles.bgvideoStyle}
+                    source={require("../../res/video/vistaraintro.mp4")}
+                    resizeMode="cover"
+                    isLooping
+                    isMuted
+                    shouldPlay
+                />
+                {/* <Image style={styles.tinyLogo} source={require('../../res/images/UK.png')}/>
+                <Text style={{color:'#fff'}}>Vistara</Text> */}
+            </View>
+            <View style={styles.bottomView}>
+                <View style={{padding: 40}}>
+                    <Text></Text>
+                </View>
+            </View>
+            {/* <View style={styles.footer}>
+                <Text>Footer</Text>
+            </View> */}
+
         </View>
     );
 };
@@ -20,18 +39,37 @@ const InitialScreen = () =>{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#47143d',
-        alignItems: 'center'
-    },
-    textStyle: {
-        color: '#fff',
-        fontSize: 30,
-        fontWeight: '300'
+        backgroundColor: '#271625'
     },
     tinyLogo: {
-        width: 110,
-        height: 110,
-        marginTop: screenheight * 15/100
+        width: screenheight * 0.135,
+        height: screenheight * 0.135,
+        marginTop: screenheight * 0.09
+    },
+    nameLogoStyle: {
+        width: screenheight * 0.209,
+        height: screenheight * 0.03,
+    },
+    header: {
+        flex: 1
+    },
+    // footer: {
+    //     flex: 1,
+    //     backgroundColor: '#d6c1b8',
+    //     borderTopLeftRadius: 30,
+    //     borderTopRightRadius: 30,
+    //     paddingVertical: 50,
+    //     paddingHorizontal:30
+    // },
+    bgvideoStyle: {
+         height: '100%'
+    },
+    bottomView: {
+        flex: 1.6,
+        backgroundColor: '#271625',
+        bottom: 45,
+        borderTopStartRadius: 50,
+        borderTopEndRadius: 50
     }
 });
 
