40323155d649f9ee34826974e03cb54aa7629406
yash <yaswanth.k23@gmail.com>
2021-05-25 21:24:17 +0530

    minor changes in registration screen

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -7,12 +7,18 @@ import {
 
 const RegistrationScreen = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text> Registration Screen</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default RegistrationScreen;
