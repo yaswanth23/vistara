ac3733c7a67df8ed3f45e157867643e5fabb6464
yash <yaswanth.k23@gmail.com>
2021-05-28 00:16:14 +0530

    flightscreen changes

--- src/screens/FlightSearchScreen.js
+++ src/screens/FlightSearchScreen.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, ImageBackground , StyleSheet, Dimensions} from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+const screenheight = Dimensions.get('window').height;
 
 const FlightSearchScreen = () => {
     return (
         <View style={styles.container}>
-            <View></View>
-            <View></View>
+            <View style={styles.header}>
+            <ImageBackground 
+                source={require('../../res/images/world.png')} 
+                style={{width: '100%', height: '100%', borderBottomLeftRadius: 95, overflow: 'hidden'}}
+            >
+                <View style={{
+                    position: 'absolute',
+                    marginTop: screenheight * 0.06,
+                    marginLeft: '5%',
+                    justifyContent: 'center'
+                }}>
+                    <Text style={styles.headerTextStyle}>Book your</Text>
+                    <Text style={styles.headerTextStyle}>Flight</Text>
+                </View>
+            </ImageBackground>
+            </View>
+            <View style={styles.footer}></View>
         </View>
     );
 };
@@ -14,6 +32,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff'
+    },
+    header: {
+        flex: 0.9,
+        backgroundColor: '#271625',
+        borderBottomLeftRadius: 100,
+    },
+    footer: {
+        flex: 1.5,
+        backgroundColor: '#fff'
+    },
+    headerTextStyle: {
+        fontSize: 35,
+        color: '#fff',
+        fontWeight: '700'
     }
 });
 
