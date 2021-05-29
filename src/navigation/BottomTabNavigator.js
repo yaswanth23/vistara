fb9c778edca33347ec6170a1d935ec1564853b3a
yash <yaswanth.k23@gmail.com>
2021-05-29 10:28:27 +0530

    navigation changes

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -3,9 +3,9 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FlightSearchScreen from '../screens/FlightSearchScreen'
 import MyProfileScreen from '../screens/MyProfileScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import TabBar from '../components/TabBar';
 import CheckinScreen from '../screens/CheckinScreen';
 import MyTripsScreen from '../screens/MyTripsScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 function FlightScreen() {
   return (
@@ -35,27 +35,31 @@ const Tab = createBottomTabNavigator();
 export default function BottomTabNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBar={(props) => <TabBar{...props} />}>
-        <Tab.Screen 
-          name="Flight"
-          component={FlightScreen}
-          initialParams={{icon: 'ios-airplane-sharp'}}
-        />
-        <Tab.Screen 
-          name="My Trips"
-          component={TripsScreen}
-          initialParams={{icon: 'ios-briefcase'}}
-        />
-        <Tab.Screen 
-          name="Check-In"
-          component={CheckScreen}
-          initialParams={{icon: 'ios-barcode-outline'}}
-        />
-        <Tab.Screen 
-          name="Profile"
-          component={ProfileScreen}
-          initialParams={{icon: 'ios-person-sharp'}}
-        />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Flight') {
+              iconName = focused ? 'ios-airplane-sharp' : 'ios-airplane-outline';
+            } else if (route.name === 'My Trips') {
+              iconName = focused ? 'ios-briefcase' : 'ios-briefcase';
+            } else if (route.name === 'Check-In') {
+              iconName = focused ? 'ios-barcode-outline' : 'ios-barcode-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'ios-person-sharp' : 'ios-person-sharp';
+            }
+            return <Ionicons name={iconName} size={23} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#47143d',
+          inactiveTintColor: '#d6c1b8',
+        }}
+      >
+        <Tab.Screen name="Flight" component={FlightScreen} />
+        <Tab.Screen name="My Trips" component={TripsScreen} />
+        <Tab.Screen name="Check-In" component={CheckScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
