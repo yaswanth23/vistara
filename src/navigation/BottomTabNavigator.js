f0278ddcb40725431bdfbb9a0d699dbfa69299b6
yash <yaswanth.k23@gmail.com>
2021-05-29 19:48:59 +0530

    tab bar animation

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import {View, Text,StyleSheet, Animated, Dimensions} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import 'react-native-gesture-handler';
 import FlightSearchScreen from '../screens/FlightSearchScreen'
 import MyProfileScreen from '../screens/MyProfileScreen';
-import { NavigationContainer } from '@react-navigation/native';
 import CheckinScreen from '../screens/CheckinScreen';
 import MyTripsScreen from '../screens/MyTripsScreen';
-import { Ionicons } from '@expo/vector-icons';
 
 function FlightScreen() {
   return (
@@ -24,22 +26,30 @@ function CheckScreen() {
     <CheckinScreen />
   );
 }
+
 function ProfileScreen() {
   return (
     <MyProfileScreen />
   );
 }
 
+function getWidth() {
+  let width = Dimensions.get("window").width
+  width = width - 60
+  return width / 5
+}
+
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
+  const tabOffsetvalue = useRef(new Animated.Value(0)).current;
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
+          tabBarIcon: ({ focused, color }) => {
             let iconName;
-            if (route.name === 'Flight') {
+            if (route.name === 'Flights') {
               iconName = focused ? 'ios-airplane-sharp' : 'ios-airplane-sharp';
             } else if (route.name === 'My Trips') {
               iconName = focused ? 'ios-briefcase' : 'ios-briefcase';
@@ -48,22 +58,113 @@ export default function BottomTabNavigator() {
             } else if (route.name === 'Profile') {
               iconName = focused ? 'ios-person-sharp' : 'ios-person-sharp';
             }
-            return <Ionicons name={iconName} size={23} color={color} />;
+            return <Ionicons name={iconName} size={20} color={color} />;
           },
         })}
         tabBarOptions={{
           activeTintColor: '#47143d',
-          inactiveTintColor: '#d6c1b8',
+          inactiveTintColor: '#b3b3b3',
+          labelStyle: {
+            fontSize: 11
+          },
+          labelStyle: {
+            fontSize: 12,
+            fontWeight: '500'
+          },
+          style: {
+            position: 'absolute',
+            elevation: 0,
+            backgroundColor: '#fff',
+            borderTopRightRadius: 15,
+            borderTopLeftRadius: 15,
+            borderTopWidth: 0,
+            height: 56,
+            alignItems:'center',
+            justifyContent: 'center',
+            paddingTop: 8,
+            paddingBottom: 10,
+            ...styles.shadow
+          }
         }}
       >
-        <Tab.Screen name="Flight" component={FlightScreen} />
-        <Tab.Screen name="My Trips" component={TripsScreen} />
-        <Tab.Screen name="Check-In" component={CheckScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen 
+          name="Flights"
+          component={FlightScreen}
+          listeners={({navigation,route}) => ({
+            tabPress: e => {
+              Animated.spring(tabOffsetvalue, {
+                toValue: 0,
+                useNativeDriver: true
+              }).start();
+            }
+          })}
+        />
+        <Tab.Screen 
+          name="My Trips" 
+          component={TripsScreen}
+          listeners={({navigation,route}) => ({
+            tabPress: e => {
+              Animated.spring(tabOffsetvalue, {
+                toValue: getWidth() * 1.5,
+                useNativeDriver: true
+              }).start();
+            }
+          })}
+        />
+        <Tab.Screen 
+          name="Check-In" 
+          component={CheckScreen}
+          listeners={({navigation,route}) => ({
+            tabPress: e => {
+              Animated.spring(tabOffsetvalue, {
+                toValue: getWidth() * 3,
+                useNativeDriver: true
+              }).start();
+            }
+          })}
+        />
+        <Tab.Screen 
+          name="Profile" 
+          component={ProfileScreen}
+          listeners={({navigation,route}) => ({
+            tabPress: e => {
+              Animated.spring(tabOffsetvalue, {
+                toValue: getWidth() * 4.4,
+                useNativeDriver: true
+              }).start();
+            }
+          })}
+        />
       </Tab.Navigator>
+      <Animated.View style={{
+        width: getWidth()-7,
+        height: 2,
+        backgroundColor: '#47143d',
+        position: 'absolute',
+        bottom: 54,
+        left: 18,
+        transform: [
+          {translateX: tabOffsetvalue}
+        ]
+      }}>
+
+      </Animated.View>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.06,
+    shadowRadius: 3.5,
+    elevation: 5
+  }
+});
 //backgroundColor: '#997588',
 //backgroundColor: '#f7eee7',
 //backgroundColor: '#d6c1b8',
--- src/screens/FlightSearchScreen.js
+++ src/screens/FlightSearchScreen.js
@@ -8,20 +8,10 @@ const FlightSearchScreen = () => {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-            <ImageBackground 
-                source={require('../../res/images/world.png')} 
-                style={{width: '100%', height: '100%', borderBottomLeftRadius: 95, overflow: 'hidden'}}
-            >
-                <View style={{
-                    position: 'absolute',
-                    marginTop: screenheight * 0.06,
-                    marginLeft: '5%',
-                    justifyContent: 'center'
-                }}>
+                
                     <Text style={styles.headerTextStyle}>Book your</Text>
                     <Text style={styles.headerTextStyle}>Flight</Text>
-                </View>
-            </ImageBackground>
+                
             </View>
             <View style={styles.footer}></View>
         </View>
@@ -31,16 +21,13 @@ const FlightSearchScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff'
     },
     header: {
         flex: 0.9,
-        backgroundColor: '#271625',
-        borderBottomLeftRadius: 100,
+        backgroundColor: '#47143d'
     },
     footer: {
         flex: 1.5,
-        backgroundColor: '#fff'
     },
     headerTextStyle: {
         fontSize: 35,
