5ecdd46e567f7e1b109846441fc2ac533cb39284
yash <yaswanth.k23@gmail.com>
2021-06-14 17:26:45 +0530

    minor changes

--- src/countryScreens/Singapore.js
+++ src/countryScreens/Singapore.js
@@ -2,15 +2,32 @@ import React from 'react';
 import {
     View,
     StyleSheet,
-    Text
+    Text,
+    Dimensions,
+    Image,
+    TouchableOpacity,
+    Animated,
+    Platform
 } from 'react-native';
+import { BlurView, VibrancyView } from "@react-native-community/blur";
+import  trendingDestinations  from "../constants/PopularDestinationData";
+import * as Haptics from 'expo-haptics';
+
+const HEADER_HEIGHT = 350;
 
 const Singapore = () => {
     return(
-        <View>
-            <Text>hi</Text>
+        <View style={styles.container}>
+            <Animated.FlatList />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    }
+});
 
 export default Singapore;
