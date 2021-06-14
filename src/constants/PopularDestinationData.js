5d4f5db1e22635f62bf7e16ec5bc5396d5f62398
yash <yaswanth.k23@gmail.com>
2021-06-14 19:49:25 +0530

    made changes in singapore screen

--- src/constants/PopularDestinationData.js
+++ src/constants/PopularDestinationData.js
@@ -7,24 +7,6 @@ const trendingDestinations = [
         image: images.Singapore1,
         placeName: "Marina Bay Sands",
         location: "Singapore",
-        viewers: [
-            {
-                id: 1,
-                profilePic: images.UserProfile1
-            },
-            {
-                id: 2,
-                profilePic: images.UserProfile2
-            },
-            {
-                id: 3,
-                profilePic: images.UserProfile3
-            },
-            {
-                id: 4,
-                profilePic: images.UserProfile3
-            }
-        ]
     },
     {
         id: "two",
@@ -32,24 +14,6 @@ const trendingDestinations = [
         image: images.Male3,
         placeName: "Fuvahmulah",
         location: "Malé, Maldives",
-        viewers: [
-            {
-                id: 1,
-                profilePic: images.UserProfile1
-            },
-            {
-                id: 2,
-                profilePic: images.UserProfile2
-            },
-            {
-                id: 3,
-                profilePic: images.UserProfile3
-            },
-            {
-                id: 4,
-                profilePic: images.UserProfile3
-            }
-        ]
     },
     {
         id: "three",
@@ -57,24 +21,6 @@ const trendingDestinations = [
         image: images.Dubai1,
         placeName: "The Burj Khalifa",
         location: "Dubai",
-        viewers: [
-            {
-                id: 1,
-                profilePic: images.UserProfile1
-            },
-            {
-                id: 2,
-                profilePic: images.UserProfile2
-            },
-            {
-                id: 3,
-                profilePic: images.UserProfile3
-            },
-            {
-                id: 4,
-                profilePic: images.UserProfile3
-            }
-        ]
     },
     {
         id: "four",
@@ -82,24 +28,6 @@ const trendingDestinations = [
         image: images.London1,
         placeName: "Tower Bridge",
         location: "London, United Kingdom",
-        viewers: [
-            {
-                id: 1,
-                profilePic: images.UserProfile1
-            },
-            {
-                id: 2,
-                profilePic: images.UserProfile2
-            },
-            {
-                id: 3,
-                profilePic: images.UserProfile3
-            },
-            {
-                id: 4,
-                profilePic: images.UserProfile3
-            }
-        ]
     },
     {
         id: "five",
@@ -107,24 +35,6 @@ const trendingDestinations = [
         image: images.Japan1,
         placeName: "Tokyo Tower",
         location: "Tokyo, Japan",
-        viewers: [
-            {
-                id: 1,
-                profilePic: images.UserProfile1
-            },
-            {
-                id: 2,
-                profilePic: images.UserProfile2
-            },
-            {
-                id: 3,
-                profilePic: images.UserProfile3
-            },
-            {
-                id: 4,
-                profilePic: images.UserProfile3
-            }
-        ]
     },
 ]
 
--- src/countryScreens/Singapore.js
+++ src/countryScreens/Singapore.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
     View,
     StyleSheet,
@@ -11,14 +11,93 @@ import {
 } from 'react-native';
 import { BlurView, VibrancyView } from "@react-native-community/blur";
 import  trendingDestinations  from "../constants/PopularDestinationData";
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 import * as Haptics from 'expo-haptics';
 
 const HEADER_HEIGHT = 350;
 
 const Singapore = () => {
+    const [selectedInfo, setSelectedInfo] = React.useState(null);
+    const navigation = useNavigation();
+
+    React.useEffect(() => {
+        setSelectedInfo(trendingDestinations)
+    },[])
+    const scrollY = useRef(new Animated.Value(0)).current;
+
+    function renderHeaderBar(){
+        return (
+            <View style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                height: 70,
+                flexDirection: 'row',
+                alignItems: 'flex-end',
+                justifyContent: 'space-between',
+                paddingHorizontal: 24,
+                paddingBottom: 10
+            }}>
+                <TouchableOpacity 
+                    style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 32,
+                        width: 32,
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        borderColor: '#F5F6FB',
+                        backgroundColor: 'rgba(2, 2, 2, 0.5)',
+                    }}
+                    activeOpacity={0.8}
+                    onPress={() => {
+                        navigation.goBack();
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                    }}
+                >
+                    <Ionicons name="ios-chevron-back" size={21} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 32,
+                        width: 32,
+                    }}
+                    activeOpacity={0.8}
+                    onPress={() => {
+                        navigation.goBack();
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                    }}
+                >
+                    <Ionicons name="ios-share-social-outline" size={22} color="black" />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return(
         <View style={styles.container}>
-            <Animated.FlatList />
+            <Animated.FlatList 
+                data={selectedInfo?.places}
+                keyExtractor={item => `${item.id}`}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View></View>
+                }
+                scrollEventThrottle={16}
+                onScroll={Animated.event([
+                    { nativeEvent: { contentOffset: { y: scrollY } } }
+                ],{ useNativeDriver: true })}
+                renderItem={({item}) => (
+                    <View>
+                        <Text>{item.id}</Text>
+                    </View>
+                )}
+            />
+            {renderHeaderBar()}
         </View>
     );
 };
