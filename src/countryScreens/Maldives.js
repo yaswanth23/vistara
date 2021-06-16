afae7d37f7a18fd61d6fabbb5231cc3d96e8e6c8
yash <yaswanth.k23@gmail.com>
2021-06-16 20:12:26 +0530

    maldives changes

--- src/countryScreens/Maldives.js
+++ src/countryScreens/Maldives.js
@@ -1,16 +1,187 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
     View,
+    Text,
     StyleSheet,
-    Text
+    Dimensions,
+    TouchableOpacity,
+    Animated,
+    FlatList,
+    StatusBar
 } from 'react-native';
+import { BlurView } from 'expo-blur';
+import  sectionData  from "../constants/SectionData";
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import * as Haptics from 'expo-haptics';
+
+const HEADER_HEIGHT = 350;
+const screenWidth = Dimensions.get('window').width;
+
+const LocationCardDetail = () => {
+    return(
+        <View></View>
+    )
+}
+
+const LocationCardInfo = () => {
+    return(
+        <BlurView
+            tint='dark'
+            style={{
+                flex: 1,
+            }}
+        >
+            <LocationCardDetail />
+        </BlurView>
+    )
+}
 
 const Maldives = () => {
+    const navigation = useNavigation();
+    const scrollY = useRef(new Animated.Value(0)).current;
+
+    function renderHeaderBar(){
+        return (
+            <View style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                height: 70,
+                flexDirection: 'row',
+                alignItems: 'flex-end',
+                justifyContent: 'space-between',
+                paddingHorizontal: 24,
+                paddingBottom: 10
+            }}>
+                <TouchableOpacity 
+                    style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 32,
+                        width: 32,
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        borderColor: '#F5F6FB',
+                        backgroundColor: 'rgba(2, 2, 2, 0.5)',
+                    }}
+                    activeOpacity={0.8}
+                    onPress={() => {
+                        navigation.goBack();
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                    }}
+                >
+                    <Ionicons name="ios-chevron-back" size={24} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 32,
+                        width: 32,
+                    }}
+                    activeOpacity={0.8}
+                    onPress={() => {
+                        navigation.goBack();
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                    }}
+                >
+                    <Ionicons name="ios-share-social-outline" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    function renderHeaderInfoSection(){
+        return(
+            <View
+                style={{
+                    marginTop: -1000,
+                    paddingTop: 1000,
+                    alignItems:'center',
+                    overflow: 'hidden',
+                    backgroundColor: '#f9ebf7',
+                    borderBottomRightRadius: 20,
+                    borderBottomLeftRadius: 20
+                }}
+            >
+                <Animated.Image 
+                    source={require('../../res/images/maldives5.jpg')}
+                    resizeMode="contain"
+                    style={{
+                        height: HEADER_HEIGHT,
+                        width: '200%',
+                        transform: [
+                            {
+                                translateY: scrollY.interpolate({
+                                    inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+                                    outputRange: [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75]
+                                })
+                            },
+                            {
+                                scale: scrollY.interpolate({
+                                    inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+                                    outputRange: [2, 1, 0.75]
+                                })
+                            }
+                        ]
+                    }}
+                />
+                <Animated.View
+                    style={{
+                        position: 'absolute',
+                        bottom: 10,
+                        left: 30,
+                        right: 30,
+                        height: 80,
+                        borderRadius: 10,
+                        overflow: 'hidden',
+                    }}
+                >
+                    <LocationCardInfo />
+                </Animated.View>
+            </View>
+        )
+    }
+
     return(
-        <View>
-            <Text>mal</Text>
+        <View style={styles.container}>
+            <StatusBar backgroundColor={'#fff'} barStyle="light-content" />
+            <Animated.FlatList 
+                data={sectionData}
+                keyExtractor={item => `${item.id}`}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent = {
+                    <View>
+                        {renderHeaderInfoSection()}
+                    </View>
+                }
+                scrollEventThrottle={16}
+                onScroll={Animated.event([
+                    { nativeEvent: { contentOffset: { y: scrollY } } }
+                ],{ useNativeDriver: true })}
+                renderItem={({item}) => (
+                    <View 
+                        style={{
+                            flexDirection: 'row',
+
+                        }}
+                    >
+                        <Text>{item.placeName}</Text>
+                    </View>
+                )}
+            />
+            {renderHeaderBar()}
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    }
+});
 
 export default Maldives;
