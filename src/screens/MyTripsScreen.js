9e25f26877e497892fd27b8a79b6e83b676d796e
yash <yaswanth.k23@gmail.com>
2021-06-28 21:27:44 +0530

    new screen changes

--- src/screens/MyTripsScreen.js
+++ src/screens/MyTripsScreen.js
@@ -1,10 +1,62 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useRef, useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Dimensions,
+    StatusBar,
+    TouchableOpacity,
+    Platform,
+    NativeModules,
+    Animated
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import * as Haptics from 'expo-haptics';
+import  trendingDestinations  from "../constants/PopularDestinationData";
+import PopularComponent from "../components/PopularComponent";
+import  sectionData  from "../constants/SectionData";
+
+const screenWidth = Dimensions.get('window').width;
+const { StatusBarManager } = NativeModules;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
 
 const MyTripsScreen = () => {
-    return (
+    const scrollY = useRef(new Animated.Value(0)).current;
+
+    function renderHeaderBar(){
+        return (
+            <View style={{
+                position: 'absolute',
+                height: STATUSBAR_HEIGHT,
+                width: screenWidth,
+                backgroundColor: '#fff'
+            }}/>
+        )
+    }
+
+    return(
         <View style={styles.container}>
-            <Text >Welcome to My Trips screen</Text>
+            <StatusBar backgroundColor={'#fff'} barStyle="dark-content" />
+            <Animated.FlatList 
+                data={sectionData}
+                keyExtractor={item => `${item.id}`}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent = {
+                    <View>
+                    </View>
+                }
+                scrollEventThrottle={16}
+                onScroll={Animated.event([
+                    { nativeEvent: { contentOffset: { y: scrollY } } }
+                ],{ useNativeDriver: true })}
+                renderItem={({item}) => (
+                    <View>
+                    </View>
+                )}
+            />
+            {renderHeaderBar()}
         </View>
     );
 };
@@ -12,9 +64,8 @@ const MyTripsScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+        backgroundColor: '#fff',
+    },
 });
 
 export default MyTripsScreen;
